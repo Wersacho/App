4ed6d2591b4834d8679fc98df2b969342d12b0d6
Wersacho <maslovskiynv@gmail.com>
2025-05-31 23:47:10 +0300

    new

---
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.app.ui.theme.AppTheme
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val fs = Firebase.firestore
+        fs.collection("books")
+            .document().set(mapOf("name" to "My fav book"))
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
---
+++ app/src/main/java/com/example/app/data/Game.kt
@@ -0,0 +1,8 @@
+package com.example.app.data
+
+data class Game(
+    val name: String,
+    val description: String,
+    val price: String,
+    val imageUrl: String
+)
